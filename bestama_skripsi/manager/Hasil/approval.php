263055509036e135a05037607c51550791e13a4f
sherlypurdayanti <143888860+sherlypurdayanti@users.noreply.github.com>
2025-05-16 18:50:13 +0700

    Delete bestama_skripsi directory

--- bestama_skripsi/manager/Hasil/approval.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../../config/koneksi.php";
-
-$id = $_GET['id'];
-$query = "UPDATE hasil_akhir SET keputusan = 'Disetujui' WHERE id_hasil = '$id'";
-mysqli_query($koneksi, $query);
-
-header("Location:../../../index.php"); // kembali ke halaman utama
-exit;
